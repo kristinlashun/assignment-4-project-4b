924da6cd02ded27bc9fc119087b5e422b242c0f6
Kristin Evans <kristinlashun@gmail.com>
2024-01-31 19:55:02 -0800

    updated box classes and box function

--- box_sort.py
+++ box_sort.py
@@ -19,22 +19,10 @@ class Box:
 
 def box_sort(box_list):
     for i in range(1, len(box_list)):
-        current_box = box_list[i]
+        current = box_list[i]
         j = i - 1
-        while j >= 0 and box_list[j].volume() < current_box.volume():
+        # Compare volumes and shift boxes
+        while j >= 0 and box_list[j].volume() < current.volume():
             box_list[j + 1] = box_list[j]
             j -= 1
-        box_list[j + 1] = current_box
-
-
-# Example usage
-b1 = Box(3.4, 19.8, 2.1)
-b2 = Box(1.0, 1.0, 1.0)
-b3 = Box(8.2, 8.2, 4.5)
-box_list = [b1, b2, b3]
-
-box_sort(box_list)
-
-
-for box in box_list:
-    print(f"Box Dimensions (LxWxH): {box.get_length()} x {box.get_width()} x {box.get_height()}, Volume: {box.volume()}")
+        box_list[j + 1] = current
